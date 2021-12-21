99d53133d7ad15c4a25acd38fdfe92edbd93961d
duc.nguyen <nguyenmanhduc406@gmail.com>
2021-12-21 13:01:20 +0700

    Create_Datacontext_NMD392

---
+++ LTQL_1721050392/Models/DataDBContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace LTQL_1721050392.Models
+{
+    public class DataDBContext : DbContext
+    {
+        public DataDBContext() : base("DataDBContext")
+        {
+        }
+
+    }
+}
